8bb5c0e7be031566f68fff233821b330ea75eee6
Daniel Smith <jellymann@gmail.com>
2013-11-07 01:40:20 +0200

    Added shader and program classes.

---
+++ athens/src/main/java/za/co/sourlemon/acropolis/athens/shaders/Program.java
@@ -0,0 +1,133 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2013 Sour Lemon.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package za.co.sourlemon.acropolis.athens.shaders;
+
+import com.hackoeur.jglm.Mat4;
+import com.hackoeur.jglm.Vec3;
+import java.io.IOException;
+import za.co.sourlemon.acropolis.athens.Resource;
+import static org.lwjgl.opengl.ARBShaderObjects.*;
+import static org.lwjgl.opengl.GL11.*;
+
+/**
+ *
+ * @author Daniel
+ */
+public class Program implements Resource
+{
+
+    private final VertexShader vertexShader;
+    private final FragmentShader fragmentShader;
+    private int program, view, projection, world, sun;
+
+    public Program(VertexShader vertexShader, FragmentShader fragmentShader)
+    {
+        this.vertexShader = vertexShader;
+        this.fragmentShader = fragmentShader;
+    }
+
+    @Override
+    public void load() throws IOException
+    {
+        if (program != 0)
+        {
+            return;
+        }
+        vertexShader.load();
+        fragmentShader.load();
+
+        program = glCreateProgramObjectARB();
+
+        if (program == 0)
+        {
+            return;
+        }
+
+        vertexShader.attachTo(program);
+        fragmentShader.attachTo(program);
+
+        glLinkProgramARB(program);
+        if (glGetObjectParameteriARB(program, GL_OBJECT_LINK_STATUS_ARB) == GL_FALSE)
+        {
+            System.err.println(getLogInfo(program));
+            program = 0;
+            return;
+        }
+
+        glValidateProgramARB(program);
+        if (glGetObjectParameteriARB(program, GL_OBJECT_VALIDATE_STATUS_ARB) == GL_FALSE)
+        {
+            System.err.println(getLogInfo(program));
+            program = 0;
+        }
+        
+        view = findUniform("view");
+        projection = findUniform("projection");
+        world = findUniform("world");
+        sun = findUniform("sun");
+    }
+
+    private int findUniform(String name)
+    {
+        return glGetUniformLocationARB(program, name);
+    }
+
+    @Override
+    public void unload()
+    {
+        vertexShader.unload();
+        fragmentShader.unload();
+    }
+    
+    public void use()
+    {
+        glUseProgramObjectARB(program);
+    }
+
+    public void setView(Mat4 mat)
+    {
+        glUniformMatrix4ARB(view, false, mat.getBuffer());
+    }
+
+    public void setProjection(Mat4 mat)
+    {
+        glUniformMatrix4ARB(projection, false, mat.getBuffer());
+    }
+
+    public void setWorld(Mat4 mat)
+    {
+        glUniformMatrix4ARB(world, false, mat.getBuffer());
+    }
+
+    public void setSun(Vec3 v)
+    {
+        glUniform3ARB(sun, v.getBuffer());
+    }
+
+    private static String getLogInfo(int obj)
+    {
+        return glGetInfoLogARB(obj, glGetObjectParameteriARB(obj, GL_OBJECT_INFO_LOG_LENGTH_ARB));
+    }
+
+}
---
+++ athens/src/main/java/za/co/sourlemon/acropolis/athens/shaders/Shader.java
@@ -0,0 +1,131 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2013 Sour Lemon.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package za.co.sourlemon.acropolis.athens.shaders;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import za.co.sourlemon.acropolis.athens.Resource;
+import static org.lwjgl.opengl.ARBShaderObjects.*;
+import static org.lwjgl.opengl.GL11.*;
+
+/**
+ *
+ * @author Daniel
+ */
+public class Shader implements Resource
+{
+
+    private int shader = 0;
+    private final File file;
+    private final int type;
+
+    public Shader(File file, int type)
+    {
+        this.file = file;
+        this.type = type;
+    }
+
+    @Override
+    public void load()
+            throws IOException
+    {
+        if (shader != 0)
+        {
+            return;
+        }
+        try
+        {
+            shader = glCreateShaderObjectARB(type);
+
+            if (shader == 0)
+            {
+                return;
+            }
+
+            glShaderSourceARB(shader, readFileAsString(file));
+            glCompileShaderARB(shader);
+
+            if (glGetObjectParameteriARB(shader, GL_OBJECT_COMPILE_STATUS_ARB) == GL_FALSE)
+            {
+                throw new IOException("Error creating shader: " + getLogInfo(shader));
+            }
+
+        } catch (IOException exc)
+        {
+            unload();
+            throw exc;
+        }
+    }
+
+    @Override
+    public void unload()
+    {
+        glDeleteObjectARB(shader);
+    }
+
+    private static String getLogInfo(int obj)
+    {
+        return glGetInfoLogARB(obj, glGetObjectParameteriARB(obj, GL_OBJECT_INFO_LOG_LENGTH_ARB));
+    }
+
+    static String readFileAsString(File file)
+            throws IOException
+    {
+        String input = "";
+
+        BufferedReader br = null;
+        try
+        {
+            br = new BufferedReader(new FileReader(file));
+
+            String line;
+            while ((line = br.readLine()) != null)
+            {
+                input += line + "\n";
+            }
+        } catch (IOException e)
+        {
+            if (br != null)
+            {
+                try
+                {
+                    br.close();
+                } catch (IOException ex)
+                {
+                }
+            }
+
+            return null;
+        }
+
+        return input;
+    }
+    
+    public void attachTo(int program)
+    {
+        glAttachObjectARB(program, shader);
+    }
+}
