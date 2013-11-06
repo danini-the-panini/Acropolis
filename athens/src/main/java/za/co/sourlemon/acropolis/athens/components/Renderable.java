5713563782b4495523a0a88dcc2291f3a976840d
Daniel Smith <jellymann@gmail.com>
2013-11-06 02:25:19 +0200

    Grabbed Renerable component and node from AFK source.

---
+++ athens/src/main/java/za/co/sourlemon/acropolis/athens/components/Renderable.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2013 Triforce - in association with the University of Pretoria and Epi-Use <Advance/>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+ package za.co.sourlemon.acropolis.athens.components;
+
+import com.hackoeur.jglm.Vec3;
+
+/**
+ *
+ * @author daniel
+ */
+public class Renderable {
+    
+    public String type = "";
+    public Vec3 colour = Vec3.VEC3_ZERO;
+    public float opacity = 1.0f;
+
+    public Renderable()
+    {
+    }
+
+    public Renderable(String type, Vec3 colour, float opacity)
+    {
+        this.type = type;
+        this.colour = colour;
+        this.opacity = opacity;
+    }
+    
+}
